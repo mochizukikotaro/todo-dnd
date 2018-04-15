5482bb87f007220d37a18897a9607119e075c9ab
Kotaro Mochizuki <mochizuki@basicinc.jp>
2018-04-15 18:28:02 +0900

    Add thead and tbody

--- app/javascript/packs/components/TodoList.jsx
+++ app/javascript/packs/components/TodoList.jsx
@@ -5,10 +5,14 @@ import TodoItem from './TodoItem'
 const TodoList = (props) => {
   return (
     <table>
-      <tr><th>やること</th><th>作成日</th></tr>
-      {props.todos.map((todo) =>
-        <TodoItem todo={todo} key={todo.name} />
-      )}
+      <thead>
+        <tr><th>やること</th><th>作成日</th></tr>
+      </thead>
+      <tbody>
+        {props.todos.map((todo) =>
+          <TodoItem todo={todo} key={todo.name} />
+        )}
+      </tbody>
     </table>
   )
 }
