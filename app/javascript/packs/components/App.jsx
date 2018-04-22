8485b4804e37401d994a282996154642d075fa19
Kotaro Mochizuki <mochizuki@basicinc.jp>
2018-04-22 18:17:14 +0900

    Add form

--- app/javascript/packs/components/App.jsx
+++ app/javascript/packs/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import TodoForm from './TodoForm'
 import TodoItem from './TodoItem'
 
 // fake data generator
@@ -46,11 +47,25 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: getItems(10)
+      items: getItems(10),
+      todos: []
     };
+    this.deleteItem = this.deleteItem.bind(this)
     this.onDragEnd = this.onDragEnd.bind(this);
   }
-
+  componentDidMount() {
+    fetch("http://localhost:3000/todos.json")
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          todos: data.map(d => ({
+            id: d.id,
+            name: d.name,
+            createdAt: d.created_at
+          }))
+        })
+      })
+  }
   onDragEnd(result) {
     // dropped outside the list
     if (!result.destination) {
@@ -67,42 +82,85 @@ export default class App extends Component {
       items
     });
   }
+  addItem(name, createdAt) {
+    const headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+    const todo = {name: name, createdAt: createdAt}
+    const method = 'POST'
+    const body = JSON.stringify(todo)
+    fetch("http://localhost:3000/todos.json", {
+      headers: headers,
+      method: method,
+      body: body
+    }).then(response => response.json())
+      .then(data => {
+        const newItem = {
+          id: data.id,
+          name: data.name,
+          createdAt: data.created_at
+        }
+        this.setState({todos: this.state.todos.concat(newItem)})
+      })
+  }
+  deleteItem(e) {
+    const id = e.target.value
+    const headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+    const method = 'DELETE'
+    fetch(`http://localhost:3000/todos/${id}.json`, {
+      headers: headers,
+      method: method
+    }).then(response => response.json())
+      .then(data => {
+        const deletedId = data.id
+        this.setState({todos: this.state.todos.filter(d => d.id != deletedId)})
+      })
+  }
 
   // Normally you would want to split things out into separate components.
   // But in this example everything is just done in one place for simplicity
   render() {
     return (
-      <DragDropContext onDragEnd={this.onDragEnd}>
-        <Droppable droppableId="droppable">
-          {(provided, snapshot) => (
-            <div
-              ref={provided.innerRef}
-              style={getListStyle(snapshot.isDraggingOver)}
-              >
-              {this.state.items.map((item, index) => (
-                <Draggable key={item.id} draggableId={item.id} index={index}>
-                  {(provided, snapshot) => (
-                    <div
-                      ref={provided.innerRef}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                      style={getItemStyle(
-                        snapshot.isDragging,
-                        provided.draggableProps.style
-                      )}
-                      >
-                      <div>{item.id}</div>
-                      <div>{item.name}</div>
-                      <div>{item.createdAt}</div>
-                    </div>
-                  )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
+      <div>
+        <h1>DnD Todo</h1>
+        <TodoForm add={(name, createdAt) => this.addItem(name, createdAt)} />
+        <DragDropContext onDragEnd={this.onDragEnd}>
+          <Droppable droppableId="droppable">
+            {(provided, snapshot) => (
+              <div
+                ref={provided.innerRef}
+                style={getListStyle(snapshot.isDraggingOver)}
+                >
+                {this.state.todos.map((item, index) => (
+                  <Draggable key={item.id} draggableId={item.id} index={index}>
+                    {(provided, snapshot) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        style={getItemStyle(
+                          snapshot.isDragging,
+                          provided.draggableProps.style
+                        )}
+                        >
+                        <div>{item.id}</div>
+                        <div>{item.name}</div>
+                        <div>{item.createdAt}</div>
+                        <div><button value={item.id} onClick={this.deleteItem}>delete</button></div>
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </div>
     );
   }
 }
